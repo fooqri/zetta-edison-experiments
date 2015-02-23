d5cfee0f212716629e87f39627c170725cbb6a7d
Patrick Goddi <patrick.goddi@gmail.com>
2015-02-23 11:41:38 -0800

    added config file

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
+// Require Zetta Drivers
+var zettaConfig = require('./zetta_config.js');
+
 var zetta = require('zetta');
 var led = require('zetta-led-edison-driver');
 var bean = require('zetta-bean-driver');
 var Hue = require('zetta-hue-driver');
 var Color = require('color');
 
+
+// Require custom apps
 var beanApp = require('./apps/bean');
 var ledApp = require('./apps/led');
 var hueApp = require('./apps/hue');
 
 
+
+
+// Initialize Zeta and specify drivers and apps to use
 zetta()
-  .name('Fooqri-Jones')
+  .name(zettaConfig.name)
   .use(bean, "Bean")
   .use(led, 13)
   .use(Hue)
   .use(beanApp)
   .use(ledApp)
   .use(hueApp)
-  .link('http://rwx-zetta.herokuapp.com/')
-  .listen(1337, function(){
+  .link(zettaConfig.serverURI)
+  .listen(zettaConfig.port, function(){
      console.log('Zetta is running at http://127.0.0.1:1337');
 });  
 
---
+++ zetta_config.js
@@ -0,0 +1,4 @@
+var name = 'home-hub';
+var serverURI = 'http://rwx-zetta.herokuapp.com/';
+var port = 1337;
+
